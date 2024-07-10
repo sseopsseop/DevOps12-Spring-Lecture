02830642f8e4acd198837a0cf4b0dbf5a22d6ea8
BIT\BITCAMP <ponde119@outlook.com>
2024-07-10 18:39:19 +0900

    24.07.10 이준섭 3일차 수업자료 MySql과 Spring 연동, Service와 DAO, DTO 역할을 나누어 로직 작성

---
+++ _09_Spring_Business_Component/src/main/java/com/bit/springboard/service/MemberServiceRun.java
@@ -0,0 +1,35 @@
+package com.bit.springboard.service;
+
+import com.bit.springboard.dto.MemberDto;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MemberServiceRun {
+    public static void main(String[] args) {
+        AbstractApplicationContext factory =
+                new GenericXmlApplicationContext("root-context.xml");
+
+        MemberService memberService = factory.getBean("memberServiceImpl", MemberService.class);
+
+        MemberDto memberDto = new MemberDto();
+
+        memberDto.setUsername("bitcamp2");
+        memberDto.setPassword("dkdlxl");
+        memberDto.setNickname("비트캠프2");
+        memberDto.setEmail("bitcamp@bit.co.kr");
+        memberDto.setTel("010-1111-1111");
+
+        memberService.join(memberDto);
+
+        memberService.getMembers().forEach(member -> {
+            System.out.println(member);
+        });
+
+        MemberDto memberDto2 = new MemberDto();
+        memberDto2.setUsername("bitcamp");
+
+        System.out.println(memberService.getMemberByUsername(memberDto2));
+
+        factory.close();
+    }
+}
