124d98e0e6e8535fb4a495d5783671460a99404e
BIT\BITCAMP <ponde119@outlook.com>
2024-07-22 17:24:07 +0900

    24.07.22 이준섭 11일차 게시글에 이미지 파일 업로드 기능 추가 및 삭제 구현

--- _21_Spring_Board_Fileupload/src/main/java/com/bit/springboard/controller/BoardController.java
+++ _21_Spring_Board_Fileupload/src/main/java/com/bit/springboard/controller/BoardController.java
@@ -8,7 +8,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -60,11 +62,12 @@ public class BoardController {
     }
 
     @GetMapping("/free-detail.do")
-    public String freeDetailView(/*HttpServletRequest reque st*//*@RequestParam("id") int id*/BoardDto boardDto, Model model) {
+    public String freeDetailView(/*HttpServletRequest request*//*@RequestParam("id") int id*/BoardDto boardDto, Model model) {
 //        int id = Integer.valueOf(request.getParameter("id"));
         boardService = applicationContext.getBean("freeBoardServiceImpl", BoardService.class);
 //        boardService.updateCnt(boardDto.getId());
         model.addAttribute("freeBoard", boardService.getBoard(boardDto.getId()));
+        model.addAttribute("fileList", boardService.getBoardFileList(boardDto.getId()));
 
         return "board/free-detail";
     }
@@ -74,7 +77,22 @@ public class BoardController {
         boardService = applicationContext.getBean("noticeServiceImpl", BoardService.class);
 
         cri.setAmount(9);
-        model.addAttribute("noticeList", boardService.getBoardList(searchMap, cri));
+
+        List<Map<String, Object>> noticeList = new ArrayList<>();
+
+        boardService.getBoardList(searchMap, cri).forEach(boardDto -> {
+            List<BoardFileDto> boardFileDtoList = boardService.getBoardFileList(boardDto.getId());
+
+            Map<String, Object> map = new HashMap<>();
+            map.put("boardDto", boardDto);
+
+            if (boardFileDtoList.size() > 0)
+                map.put("file", boardFileDtoList.get(0));
+
+            noticeList.add(map);
+        });
+
+        model.addAttribute("noticeList", noticeList);
         model.addAttribute("searchMap", searchMap);
 
         int total = boardService.getBoardTotalCnt(searchMap);
@@ -88,6 +106,7 @@ public class BoardController {
     public String noticeDetailView(BoardDto boardDto, Model model) {
         boardService = applicationContext.getBean("noticeServiceImpl", BoardService.class);
         model.addAttribute("notice", boardService.getBoard(boardDto.getId()));
+        model.addAttribute("fileList", boardService.getBoardFileList(boardDto.getId()));
         return "board/notice-detail";
     }
 
@@ -103,7 +122,7 @@ public class BoardController {
     }
 
     @PostMapping("/post.do")
-    public String post(BoardDto boardDto) {
+    public String post(BoardDto boardDto, MultipartFile[] uploadFiles) {
         // 게시판 타입에 따른 동적 의존성 주입
         if(boardDto.getType().equals("free")) {
             boardService = applicationContext.getBean("freeBoardServiceImpl", BoardService.class);
@@ -111,7 +130,7 @@ public class BoardController {
             boardService = applicationContext.getBean("noticeServiceImpl", BoardService.class);
         }
 
-        boardService.post(boardDto);
+        boardService.post(boardDto, uploadFiles);
 
         if(boardDto.getType().equals("free")) {
             return "redirect:/board/free-list.do";
@@ -155,20 +174,30 @@ public class BoardController {
     @PostMapping("/notice-list-ajax.do")
     @ResponseBody
     public Map<String, Object> noticeListAjax(@RequestParam Map<String, String> searchMap, Criteria cri) {
-        System.out.println(cri.getAmount());
-//        Arrays.stream(amountArr).forEach(System.out::println);
         boardService = applicationContext.getBean("noticeServiceImpl", BoardService.class);
-        List<BoardDto> noticeList = boardService.getBoardList(searchMap, cri);
+
+//        cri.setAmount(9);
+
+        List<Map<String, Object>> noticeList = new ArrayList<>();
+
+        boardService.getBoardList(searchMap, cri).forEach(boardDto -> {
+            List<BoardFileDto> boardFileDtoList = boardService.getBoardFileList(boardDto.getId());
+
+            Map<String, Object> map = new HashMap<>();
+
+            map.put("boardDto", boardDto);
+            if(boardFileDtoList.size() > 0){
+                map.put("file", boardFileDtoList.get(0));
+            }
+            noticeList.add(map);
+        });
 
         Map<String, Object> returnMap = new HashMap<>();
+
         returnMap.put("noticeList", noticeList);
 
         return returnMap;
     }
-
-
-
-
 
 
 
